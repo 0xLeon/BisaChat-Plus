94e932887f6dddeb83908e259069aaeeefce6ebd
Stefan Hahn <Stefan_Hahn1@gmx.net>
2011-05-03 00:07:19 +0200

    moved bind.js to header.js to make sure bind-functions will b available in every tool and module

--- header.js
+++ header.js
@@ -31,3 +31,32 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+/*
+ * bind and bindAsEventListener taken from Prototype
+ * Copyright (c) 2005-2010 Sam Stephenson
+ */
+
+if (typeof Function.prototype.bind === 'undefined') {
+	Function.prototype.bind = function(context) {
+		if (arguments.length < 2 && typeof arguments[0] === 'undefined') return this;
+		var __method = this, args = Array.prototype.slice.call(arguments, 1);
+		
+		return function() {
+			args = Array.prototype.slice.call(args, 0);
+			var argsLength = args.length, argumentsLength = arguments.length;
+			while (argumentsLength--)  args[argsLength + argumentsLength] = arguments[argumentsLength];
+			return __method.apply(context, args);
+		};
+	};
+}
+
+Function.prototype.bindAsEventListener = function(context) {
+	var __method = this, args = Array.prototype.slice.call(arguments, 1);
+	
+	return function(event) {
+		var array = [event || window.event];
+		var arrayLength = array.length, length = args.length;
+		while (length--) array[arrayLength + length] = args[length];
+		return __method.apply(context, array);
+	};
+};

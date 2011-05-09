2081e4e57d3a72e4023e5d0c7f089aad98f47278
Stefan Hahn <Stefan_Hahn1@gmx.net>
2011-05-09 15:18:14 +0200

    code is more flexible more

--- modules/BBCodeParser.js
+++ modules/BBCodeParser.js
@@ -52,60 +52,60 @@ var BBCodeParser = {
 	},
 	
 	parseTags: function(mString, openingTag, openingTagOption, closingTag, offset, string) {
-		if (BBCodeParser.isValidTag(openingTag)) {
-			if (BBCodeParser.noParse) return '['+openingTag+']';
+		if (this.isValidTag(openingTag)) {
+			if (this.noParse) return '['+openingTag+']';
 			
 			switch (openingTag) {
 				case 'code':
-					BBCodeParser.noParse = true;
-					BBCodeParser.openingTags.push({ BBTag: openingTag, closingTag: '</code></pre>' });
+					this.noParse = true;
+					this.openingTags.push({ BBTag: openingTag, closingTag: '</code></pre>' });
 					return '<pre><code>';
 				case 'pre':
-					BBCodeParser.openingTags.push({ BBTag: openingTag, closingTag: '</pre>' });
+					this.openingTags.push({ BBTag: openingTag, closingTag: '</pre>' });
 					return '<pre>';
 				case 'color':
 				case 'colour':
-					if (!openingTagOption || !BBCodeParser.COLOR_REGEX.test(openingTagOption)) openingTagOption = 'inherit';
-					BBCodeParser.openingTags.push({ BBTag: openingTag, closingTag: '</span>' });
+					if (!openingTagOption || !this.COLOR_REGEX.test(openingTagOption)) openingTagOption = 'inherit';
+					this.openingTags.push({ BBTag: openingTag, closingTag: '</span>' });
 					return '<span style="color: '+openingTagOption+';">';
 				case 'size':
-					if (!openingTagOption || !BBCodeParser.NUMBER_REGEX.test(openingTagOption)) openingTagOption = 1;
-					BBCodeParser.openingTags.push({ BBTag: openingTag, closingTag: '</span>' });
+					if (!openingTagOption || !this.NUMBER_REGEX.test(openingTagOption)) openingTagOption = 1;
+					this.openingTags.push({ BBTag: openingTag, closingTag: '</span>' });
 					return '<span style="font-size: '+(Math.min(Math.max(openingTagOption, 0.7), 3)*12)+'px;">';
 				case 's':
-					BBCodeParser.openingTags.push({ BBTag: openingTag, closingTag: '</span>' });
+					this.openingTags.push({ BBTag: openingTag, closingTag: '</span>' });
 					return '<span style="text-decoration: line-through;">';
 				case 'i':
-					BBCodeParser.openingTags.push({ BBTag: openingTag, closingTag: '</span>' });
+					this.openingTags.push({ BBTag: openingTag, closingTag: '</span>' });
 					return '<span style="font-style: italic;">';
 				case 'noparse':
-					BBCodeParser.noParse = true;
+					this.noParse = true;
 					return '';
 				case 'quote':
 					openingTag = 'blockquote';
 				case 'q':
 				case 'blockquote':
-					BBCodeParser.openingTags.push({ BBTag: openingTag, closingTag: '</'+openingTag+'>' });
-					return ((!!openingTagOption && !!openingTagOption.length && BBCodeParser.URI_REGEX.test(openingTagOption)) ? '<'+openingTag+' cite="'+openingTagOption+'">' : '<'+openingTag+'>');
+					this.openingTags.push({ BBTag: openingTag, closingTag: '</'+openingTag+'>' });
+					return ((!!openingTagOption && !!openingTagOption.length && this.URI_REGEX.test(openingTagOption)) ? '<'+openingTag+' cite="'+openingTagOption+'">' : '<'+openingTag+'>');
 				default:
-					BBCodeParser.openingTags.push({ BBTag: openingTag, closingTag: '</'+openingTag+'>' });
+					this.openingTags.push({ BBTag: openingTag, closingTag: '</'+openingTag+'>' });
 					return '<'+openingTag+'>';
 			}
 		}
 		
-		if (BBCodeParser.isValidTag(closingTag)) {
-			if (BBCodeParser.noParse) {
+		if (this.isValidTag(closingTag)) {
+			if (this.noParse) {
 				if (closingTag == 'noparse' || closingTag == 'code') {
-					BBCodeParser.noParse = false;
+					this.noParse = false;
 					return '';
 				}
 				
 				return '['+closingTag+']';
 			}
 			
-			if (!BBCodeParser.openingTags.length || BBCodeParser.openingTags[BBCodeParser.openingTags.length-1].BBTag != closingTag) return '<span style="color: red">[/'+closingTag+']</span>';
+			if (!this.openingTags.length || this.openingTags[this.openingTags.length-1].BBTag != closingTag) return '<span style="color: red">[/'+closingTag+']</span>';
 			
-			return BBCodeParser.openingTags.pop().closingTag;
+			return this.openingTags.pop().closingTag;
 		}
 		
 		return mString;
@@ -115,7 +115,11 @@ var BBCodeParser = {
 		var result = '';
 		var endtags = '';
 		
-		result = text.replace(this.TEXT_REGEX, this.parseTags);
+		result = text.replace(this.TEXT_REGEX, (function(context) {
+			return function() {
+				return context.parseTags.apply(context, arguments);
+			}
+		})(this));
 		
 		if (!!this.openingTags.length) {
 			while (!!this.openingTags.length) endtags += this.openingTags.pop().closingTag;

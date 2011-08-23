24d7f79ca9c75e3b90e6373878b335aeae6164b4
Stefan Hahn <Stefan_Hahn1@gmx.net>
2011-08-23 10:31:05 +0200

    Backup Module now works as exspected

--- modules/Backup.js
+++ modules/Backup.js
@@ -45,9 +45,12 @@ Modules.Backup = {
 	},
 	
 	buildOverlay: function() {
-		this.callerObj.buildOverlay('backup', './wcf/icon/dbImportL.png', 'Backup', function() {
-			return (document.createTextNode('Backup'));
-		});
+		this.callerObj.buildOverlay('backup', './wcf/icon/dbImportL.png', 'Backup', function(overlayContentNode) {
+			this.overlayContentBuilder(overlayContentNode);
+		}.bind(this),
+		function() {
+			this.overlayContentBuilder(API.w.$$('#backup .overlayContent')[0]);
+		}.bind(this));
 	},
 	
 	backupSettings: function() {
@@ -67,5 +70,68 @@ Modules.Backup = {
 				}.bind(this)
 			});
 		}
+	},
+	
+	overlayContentBuilder: function(overlayContentNode) {
+		if (typeof overlayContentNode !== 'object') throw new TypeError('overlayContentNode has to be of type object');
+		
+		GM_xmlhttpRequest({
+			method: 'GET',
+			url: 'http://projects.swallow-all-lies.com/greasemonkey/files/bisachatPlus/backup/getBackup.php?action=getList&userID='+API.w.settings.userID,
+			onload: function(transport) {
+				var xml = ((!transport.responseXML) ? (new DOMParser()).parseFromString(transport.responseText, 'text/xml') : transport.responseXML);
+				var node = null;
+				
+				if (xml.getElementsByTagName('entry').length > 0) {
+					node = new API.w.Element('div');
+					
+					API.w.$A(xml.getElementsByTagName('entry')).each(function(item) {
+						var p = new API.w.Element('p');
+						var a = new API.w.Element('a', { href: 'javascript:;' });
+						var input = new API.w.Element('input', { 'type': 'hidden', value: String(item.getElementsByTagName('index')[0].firstChild.nodeValue) });
+						var span = new API.w.Element('span');
+						
+						var backupTimeObj = new Date(Math.floor(Number(item.getElementsByTagName('timestamp')[0].firstChild.nodeValue) * 1000));
+						var backupTimeString = '';
+						
+						backupTimeString += backupTimeObj.getDate()+'. '+(['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'][backupTimeObj.getMonth()])+' '+backupTimeObj.getFullYear()+', ';
+						backupTimeString += ((backupTimeObj.getHours() < 10) ? '0' : '')+backupTimeObj.getHours()+':'+((backupTimeObj.getMinutes() < 10) ? '0' : '')+backupTimeObj.getMinutes()+' Uhr';
+						
+						a.addEventListener('click', function(event) {
+							GM_xmlhttprequest({
+								method: 'GET',
+								url: 'http://projects.swallow-all-lies.com/greasemonkey/files/bisachatPlus/backup/getBackup.php?action=getData&userID='+API.w.settings.userID+'&index='+event.target.getElementsByTagName('input')[0].getAttribute('value'),
+								headers: {
+									'Accept': 'application/json'
+								},
+								onload: function(innerTransport) {
+									API.Storage.importSettings(innerTransport.responseJSON);
+								}
+							});
+						}, true);
+						
+						span.appendChild(document.createTextNode('Datensicherung vom '+backupTimeString));
+						a.appendChild(input);
+						a.appendChild(span);
+						p.appendChild(a);
+						node.appendChild(p);
+					});
+				}
+				else {
+					node = new API.w.Element('p');
+					
+					node.appendChild(document.createTextNode('Keine Datensicherungsen auf dem Server vorhanden.'));
+				}
+				
+				if (!!overlayContentNode.firstChild) {
+					overlayContentNode.replaceChild(node, overlayContentNode.firstChild);
+				}
+				else {
+					overlayContentNode.appendChild(node);
+				}
+			}
+		});
+		
+		return null;
 	}
 };

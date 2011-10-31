7ce6a38bebdd9eea06616db56c924449b976472d
Stefan Hahn <Stefan_Hahn1@gmx.net>
2011-10-31 14:32:49 +0100

    new module 'AutoAway' which can set away status after an user-defined time of inactivity

---
+++ modules/AutoAway.js
@@ -0,0 +1,49 @@
+/* 
+ * Auto Away Module
+ * Copyright (C) 2011 Stefan Hahn
+ */
+Modules.AutoAway = {
+	callerObj: null,
+	timerHandle: null,
+	
+	init: function(callerObj) {
+		this.callerObj = callerObj;
+		
+		if (API.Storage.getValue('autoAwayTimeoutStatus', true)) this.startTimer();
+		this.registerOptions();
+	},
+	
+	registerOptions: function() {
+		this.callerObj.registerTextOption('autoAwayTimeout', 'Zeit bis zum Away-Status in Minuten', 5, function(newValue) {
+			this.startTimer();
+		}, this);
+		this.callerObj.registerMessagePrefilter('autoAway', 'Auto Away', 'Auto Away aktivieren', 'a', true, function(event, checked, nickname, message, messageType, ownMessage) {
+			if (checked && ownMessage) {
+				this.startTimer();
+			}
+		}, function(event, checked) {
+			if (checked) {
+				this.startTimer();
+			}
+			else {
+				this.stopTimer();
+			}
+			
+			return true;
+		}, this);
+	},
+	
+	startTimer: function() {
+		this.stopTimer();
+		this.timerHandle = API.w.setTimeout(function() {
+			this.callerObj.pushMessage('/away');
+		}.bind(this), API.Storage.getValue('autoAwayTimeoutValue', 5)*60000);
+	},
+	
+	stopTimer: function() {
+		if (this.timerHandle !== null) {
+			API.w.clearTimeout(this.timerHandle);
+			this.timerHandle = null;
+		}
+	}
+};

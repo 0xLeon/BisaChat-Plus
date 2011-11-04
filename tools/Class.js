a93fe6e62872b251dc8edac574c6600b421244fd
Stefan Hahn <Stefan_Hahn1@gmx.net>
2011-11-04 21:32:45 +0100

    new Class implementetion, needed for class-based OOP

---
+++ tools/Class.js
@@ -0,0 +1,60 @@
+var Class = function() {
+	var parent = null, properties = API.w.$A(arguments);
+	
+	if (typeof properties[0] === 'function') parent = properties.shift();
+	
+	function klass() {
+		this.initialize.apply(this, arguments);
+	}
+	
+	Object.extend(klass, {
+		addMethods: function(source) {
+			var ancestor = this.superclass && this.superclass.prototype;
+			var properties = Object.keys(source);
+			
+			for (var i = 0, length = properties.length; i < length; i++) {
+				var property = properties[i];
+				var value = source[property];
+				
+				if (ancestor && (typeof value === 'function') && (value.argumentNames()[0] === '$super')) {
+					var method = value;
+					
+					value = (function(m) {
+						return function() {
+							return ancestor[m].apply(this, arguments);
+						};
+					})(property).wrap(method);
+					
+					value.valueOf = method.valueOf.bind(method);
+					value.toString = method.toString.bind(method);
+				}
+				
+				this.prototype[property] = value;
+			}
+			
+			return this;
+		}
+	});
+	klass.superclass = parent;
+	klass.subclasses = [];
+	
+	if (parent) {
+		var subclass = function() { };
+		
+		subclass.prototype = parent.prototype;
+		klass.prototype = new subclass;
+		parent.subclasses.push(klass);
+	}
+	
+	for (var i = 0; i < properties.length; i++) {
+		klass.addMethods(properties[i]);
+	}
+	
+	if (!klass.prototype.initialize) {
+		klass.prototype.initialize = function() {};
+	}
+	
+	klass.prototype.constructor = klass;
+	
+	return klass;
+};

0b33a29997898346e10b159162d78dabb59bb472
Stefan Hahn <Stefan_Hahn1@gmx.net>
2011-11-25 22:24:19 +0100

    removed 'open links in new tab'-listener because of changed event system has no need for this anymore

--- modules/MessagePrefilters.js
+++ modules/MessagePrefilters.js
@@ -30,9 +30,5 @@ Modules.AddOn.MessagePrefilters = new ClassSystem.Class(Modules.Util.AbstractMod
 				event.target.querySelector('span[onclick]').setAttribute('onclick', 'chat.insert(\'/team \', false)');
 			}
 		});
-		
-		Event.register('messageReceived', function(event) {
-			event.text = event.text.gsub('<a', '<a target="_blank"');
-		});
 	}
 });

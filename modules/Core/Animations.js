3b8d5b9ae630697a5cad40afc9c29a18b274e982
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-03-22 23:31:49 +0100

    added basic structure of CSS3 Animations module, which will replace all the Scriptaculous calls

---
+++ modules/Core/Animations.js
@@ -0,0 +1,9 @@
+/* 
+ * Animations Core Module
+ * Copyright (C) 2011-2012 Stefan Hahn
+ */
+Modules.Core.Animations = new ClassSystem.Class(Modules.Util.AbstractModule, {
+	addStyleRules: function() {
+		
+	}
+});

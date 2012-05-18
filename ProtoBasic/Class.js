ccba48a79b9b73eaeff4bb978b64aaf42a971a72
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-05-18 19:08:32 +0200

    fixed small syntax error

--- ProtoBasic/Class.js
+++ ProtoBasic/Class.js
@@ -10,7 +10,7 @@ var ClassSystem = (function() {
 		if (!iterable) return [];
 		if ('toArray' in Object(iterable)) return iterable.toArray();
 		
-		var length = iterable.length || 0
+		var length = iterable.length || 0;
 		var results = new Array(length);
 		
 		while (length--) results[length] = iterable[length];

000ca50cd7d6ec88808032801fe89b2a94c590fe
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-05-21 02:05:30 +0200

    new offset and layout related functions and Offset class

--- ProtoBasic/Element.js
+++ ProtoBasic/Element.js
@@ -6,7 +6,32 @@
  * Copyright (c) 2005-2010 Sam Stephenson
  */
 (function() {
-	var ELEMENT_CACHE = {};
+	var Offset = new ClassSystem.Class({
+		initialize: function(left, top) {
+			this.left = Math.round(left);
+			this.top = Math.round(top);
+
+			this[0] = this.left;
+			this[1] = this.top;
+		},
+
+		relativeTo: function(offset) {
+			return new Element.Offset(this.left - offset.left, this.top - offset.top);
+		},
+
+		inspect: function() {
+			return '<Element.Offset left: ' + this.left.toString() + ' top: ' + this.top.toString() + '>';
+		},
+
+		toString: function() {
+			return '[' + this.left.toString() + ', ' + this.top.toString() + ']';
+		},
+
+		toArray: function() {
+			return [this.left, this.top];
+		}
+	});
+	
 	function Element(tagName, attributes) {
 		attributes = attributes || {};
 		tagName = tagName.toLowerCase();
@@ -19,7 +44,7 @@
 	}
 	
 	function writeAttribute(element, name, value) {
-		//element = $(element);
+		// element = $(element);
 		var attributes = {};
 		var table = ATTRIBUTE_TRANSLATIONS.write;
 		
@@ -51,6 +76,97 @@
 		return element;
 	}
 	
+	function descendantOf(element, ancestor) {
+		// element = $(element);
+		// ancestor = $(ancestor);
+		
+		return ((element.compareDocumentPosition(ancestor) & 8) === 8);
+	}
+	
+	function getStyle(element, style) {
+		// element = $(element);
+		style = normalizeStyleName(style);
+		var value = element.style[style];
+		
+		if (!value || (value === 'auto')) {
+			var css = document.defaultView.getComputedStyle(element, null);
+			
+			value = ((css) ? (css[style]) : (null));
+		}
+
+		if (style === 'opacity') {
+			return ((value) ? (parseFloat(value)) : (1.0));
+		}
+		
+		return ((value === 'auto') ? (null) : (value));
+
+	}
+	
+	function cumulativeOffset(element) {
+		// element = $(element);
+		var valueT = 0;
+		var valueL = 0;
+		
+		if (element.parentNode) {
+			do {
+				valueT += element.offsetTop || 0;
+				valueL += element.offsetLeft || 0;
+				element = element.offsetParent;
+			} while (element);
+		}
+		
+		return new Element.Offset(valueL, valueT);
+	}
+	
+	function cumulativeScrollOffset(element) {
+		// element = $(element);
+		var valueT = 0;
+		var valueL = 0;
+		
+		do {
+			valueT += element.scrollTop || 0;
+			valueL += element.scrollLeft || 0;
+			element = element.parentNode;
+		} while (element);
+		
+		return new Element.Offset(valueL, valueT);
+	}
+	
+	function viewportOffset(forElement) {
+		var valueT = 0;
+		var valueL = 0;
+		var docBody = document.body;
+		var element = /*$(*/forElement/*)*/;
+		
+		do {
+			valueT += element.offsetTop || 0;
+			valueL += element.offsetLeft || 0;
+			
+			if ((element.offsetParent == docBody) && (Element.getStyle(element, 'position') == 'absolute')) {
+				break;
+			}
+		} while (element = element.offsetParent);
+		
+		element = forElement;
+		do {
+			if (element != docBody) {
+				valueT -= element.scrollTop || 0;
+				valueL -= element.scrollLeft || 0;
+			}
+		} while (element = element.parentNode);
+		
+		return new Element.Offset(valueL, valueT);
+	}
+	
+	function normalizeStyleName(style) {
+		if ((style === 'float') || (style === 'styleFloat')) {
+			return 'cssFloat';
+		}
+		
+		return style.camelize();
+	}
+	
+	var ELEMENT_CACHE = {};
 	var ATTRIBUTE_TRANSLATIONS = {
 		write: {
 			names: {
@@ -86,7 +202,13 @@
 	};
 	
 	Object.extend(Element, {
-		writeAttribute:	writeAttribute
+		Offset:			Offset,
+		
+		writeAttribute:		writeAttribute,
+		getStyle:		getStyle,
+		cumulativeOffset:	cumulativeOffset,
+		cumulativeScrollOffset:	cumulativeScrollOffset,
+		viewportOffset:		viewportOffset
 	});
 	
 	var oldElement = window.Element;

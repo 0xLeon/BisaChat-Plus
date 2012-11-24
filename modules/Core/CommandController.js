8595f47cbe3baf6b67162b13673de7e1b431e0e1
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-11-24 21:30:06 +0100

    Added CommandHandler core module
    This core module will handle any BC+ specific commands like stats printing or scripting engine

---
+++ modules/Core/CommandController.js
@@ -0,0 +1,68 @@
+/* 
+ * Command Controller Core Module
+ * Copyright (C) 2011-2012 Stefan Hahn
+ */
+Modules.Core.CommandController = new ClassSystem.Class(Modules.Util.AbstractCoreModule, {
+	initializeVariables: function() {
+		this.commands = $H({});
+	},
+	
+	addListeners: function() {
+		Event.register('messageSent', function(event) {
+			var parsedText = this.parseText(event.parameters.text);
+			
+			if (!!parsedText && !!this.commands.get(parsedText.command)) {
+				var command = this.commands.get(parsedText.command);
+				
+				if (Object.isString(command.action)) {
+					event.parameters.text = command.action;
+				}
+				else if (Object.isFunction(command.action)) {
+					event.parameters.text = command.action(parsedText);
+				}
+			}
+		}, this);
+	},
+	
+	addCommand: function(name, action) {
+		if (!!this.commands.get(name)) {
+			throw new Error('Command »' + name + '« already exists.')
+		}
+		
+		this.commands.set(name, action);
+	},
+	
+	removeCommand: function(name) {
+		if (!!this.commands.get(name)) {
+			throw new Error('Command »' + name + '« doesn\'t exist.')
+		}
+		
+		this.commands.unset(name);
+	},
+	
+	parseText: function(text) {
+		if (text.startsWith('/')) {
+			var command = text.slice(1, ((!text.includes(' ')) ? (text.length) : text.indexOf(' ')));
+			var parameters = ((!text.includes(' ')) ? '' : text.slice(text.indexOf(' ') + 1));
+			
+			if (parameters.includes(',')) {
+				parameters = parameters.split(',').invoke('trim'); 
+			}
+			else {
+				parameters = parameters.split(' ').invoke('trim'); 
+			}
+			
+			if ((parameters.length === 1) && (parameters[0] === '')) {
+				parameters.clear();
+			}
+			
+			return {
+				command:	command,
+				parameters:	parameters
+			};
+		}
+		else {
+			return null;
+		}
+	}
+});

fa55443d96a1b81ea556ef9f23a6189d2e45f9d5
Stefan Hahn <Stefan_Hahn1@gmx.net>
2014-08-09 13:30:22 +0200

    Added basic userscript file

---
+++ BisaChatPlus.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name           BisaChat Plus
+// @description    Add's a few more comfortable functions to BisaChat
+// @version        3.0.0dev
+// @author         Stefan Hahn
+// @copyright      2011-2014, Stefan Hahn
+// @license        GNU General Public License, version 2
+// @namespace      http://projects.swallow-all-lies.com/greasemonkey/namsespaces/bisachat.plus
+// @include        http://www.bisaboard.de/chat/index.php/Chat/*/
+// @include        https://www.bisaboard.de/chat/index.php/Chat/*/
+// ==/UserScript==
+
+(function(Window, $) {
+	"use strict";
+	
+	
+})(unsafeWindow, unsafeWindow.jQuery);

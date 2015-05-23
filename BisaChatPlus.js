dc837148b72a50383de5b5c46b956c69fd330377
Stefan <Stefan_Hahn1@gmx.net>
2015-05-23 11:29:00 +0200

    Added mutation observer on standard stream with green text for testing

--- BisaChatPlus.js
+++ BisaChatPlus.js
@@ -3,6 +3,31 @@
 	
 	var BisaChatPlus = (function() {
 		var init = function() {
+			var messageObserver = new MutationObserver(function(mutations) {
+				mutations.forEach(function(mutation) {
+					for (var i = 0, l = mutation.addedNodes.length; i < l; i++) {
+						var messageNode = $(mutation.addedNodes[i]);
+						
+						if (messageNode.hasClass('timsChatMessage')) {
+							var messageText = $.trim(messageNode.find('.timsChatText').text());
+							
+							if (messageText.startsWith('>')) {
+								messageNode.find('.timsChatText').css({
+									color: '#792'
+								});
+							}
+						}
+					}
+				});
+			});
+			var messageObserverConfig = {
+				childList: true,
+				attributes: false,
+				characterData: false
+			};
+			var messageObserverTarget = $('#timsChatMessageContainer0 > ul');
+			
+			messageObserver.observe(messageObserverTarget[0], messageObserverConfig);
 		};
 		
 		init();

de8f6f3570423fe6067b51845d2cbe34a4bedf0a
Stefan <Stefan_Hahn1@gmx.net>
2015-05-23 11:27:14 +0200

    Moved header to own file

--- BisaChatPlus.js
+++ BisaChatPlus.js
@@ -1,15 +1,3 @@
-// ==UserScript==
-// @name           BisaChat Plus
-// @description    Add's a few more comfortable functions to BisaChat
-// @version        3.0.0dev
-// @author         Stefan Hahn
-// @copyright      2011-2014, Stefan Hahn
-// @license        GNU General Public License, version 2
-// @namespace      http://projects.swallow-all-lies.com/greasemonkey/namsespaces/bisachat.plus
-// @include        http://www.bisaboard.de/chat/index.php/Chat/*/
-// @include        https://www.bisaboard.de/chat/index.php/Chat/*/
-// ==/UserScript==
-
 (function(Window, $, WCF) {
 	"use strict";
 	
---
+++ header.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name           BisaChat Plus
+// @description    Add's a few more comfortable functions to BisaChat
+// @version        {version}
+// @author         Stefan Hahn
+// @copyright      2011-2015, Stefan Hahn
+// @license        GNU General Public License, version 2
+// @namespace      http://projects.swallow-all-lies.com/greasemonkey/namsespaces/bisachat.plus
+// @include        http://chat.bisaboard.de/index.php/Chat/*/
+// @include        https://chat.bisaboard.de/index.php/Chat/*/
+// @run-at         document-end
+// ==/UserScript==

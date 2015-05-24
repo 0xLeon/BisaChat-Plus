1c6af82d4e3d3ff472052a9ec9b7ba6c8b97d0f9
Stefan <Stefan_Hahn1@gmx.net>
2015-05-24 09:38:19 +0200

    Added basic module handling

--- BisaChatPlus.js
+++ BisaChatPlus.js
@@ -2,12 +2,16 @@
 	"use strict";
 	
 	var BisaChatPlus = (function() {
+		var bcplus = null;
 		var event = {
 			messageAdded: $.Callbacks(),
 			messageSubmit: $.Callbacks()
 		};
 		
 		var init = function() {
+			bcplus = {
+				addEventListener:	addEventListener
+			};
 			var messageObserver = new MutationObserver(function(mutations) {
 				mutations.forEach(function(mutation) {
 					for (var i = 0, l = mutation.addedNodes.length; i < l; i++) {
@@ -27,6 +31,17 @@
 			var messageObserverTarget = $('#timsChatMessageContainer0 > ul');
 			
 			messageObserver.observe(messageObserverTarget[0], messageObserverConfig);
+			
+			
+			initModules();
+		};
+		
+		var initModules = function() {
+			console.log('BisachatPlus.initModules()');
+			$.each(Modules, function(moduleName, moduleObject) {
+				console.log(moduleName);
+				moduleObject.initialize(bcplus);
+			});
 		};
 		
 		var addEventListener = function(eventName, callback) {

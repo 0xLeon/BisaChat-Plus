b887d8ccc0fd43c45f47682a6c24187f5e3e0b3f
Stefan <Stefan_Hahn1@gmx.net>
2015-05-30 10:28:30 +0200

    Renamed ResizeChatWindow to UIOptimize

--- modules/UIOptimize.js
+++ modules/UIOptimize.js
@@ -1,15 +1,15 @@
-Modules.ResizeChatWindow = (function () {
+Modules.UIOptimize = (function() {
 	var bcplus = null;
 	
 	var initialize = function(_bcplus) {
-		console.log('Modules.ResizeChatWindow.initialize()');
+		console.log('Modules.UIOptimize.initialize()');
 		bcplus = _bcplus;
 		
 		addEventListeners();
 	};
 
 	var addEventListeners = function() {
-		console.log('Modules.ResizeChatWindow.addEventListeners()');
+		console.log('Modules.UIOptimize.addEventListeners()');
 		$('#timsChatSmilies').on('click', function() {
 			Window.setTimeout(function() {
 				$(Window).resize();
@@ -19,5 +19,5 @@ Modules.ResizeChatWindow = (function () {
 	
 	return {
 		initialize:	initialize
-	}
+	};
 })();

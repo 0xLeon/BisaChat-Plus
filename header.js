7c487d2b65202987b6dd323bcac9586eec25b74c
Stefan <Stefan_Hahn1@gmx.net>
2015-05-30 12:53:49 +0200

    Added String.generateUUID() util method

--- header.js
+++ header.js
@@ -17,6 +17,21 @@
 (function(Window, $, WCF) {
 	// TODO: use strict again
 	
+	/**
+	 *  @see	http://stackoverflow.com/a/8809472
+	 */
+	// TODO: move elsewhere?
+	String.generateUUID = function() {
+		var d = new Date().getTime();
+		
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = (d + Math.random() * 16) % 16 | 0;
+			d = Math.floor(d / 16);
+			
+			return ((c == 'x') ? r : (r & 0x3 | 0x8)).toString(16);
+		});
+	};
+	
 	try {
 		/*{content}*/
 	}

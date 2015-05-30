c047eb5b740d372664920627c23d3e98e17abf55
Stefan <Stefan_Hahn1@gmx.net>
2015-05-30 02:53:25 +0200

    Added @require rules to metadata block to include Waypoints library

--- header.js
+++ header.js
@@ -8,6 +8,8 @@
 // @namespace      http://projects.swallow-all-lies.com/greasemonkey/namsespaces/bisachat.plus
 // @match          http://chat.bisaboard.de/index.php/Chat/*/
 // @match          https://chat.bisaboard.de/index.php/Chat/*/
+// @require        http://projects.0xleon.de/userscripts/bcplus/resources/scripts/waypoints.noframework.min.js
+// @require        http://projects.0xleon.de/userscripts/bcplus/resources/scripts/waypoints.inview.min.js
 // @grant          none
 // @run-at         document-end
 // ==/UserScript==

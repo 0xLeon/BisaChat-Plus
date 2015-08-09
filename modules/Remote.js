8e7d8ba245a2b832de3ea1ddbd82beeb22c7f799
Stefan <Stefan_Hahn1@gmx.net>
2015-08-09 21:50:24 +1000

    Added !browser remote command

--- modules/Remote.js
+++ modules/Remote.js
@@ -10,19 +10,35 @@ Modules.Remote = (function() {
 	var addEventListeners = function() {
 		bcplus.addEventListener('messageReceived', function(message) {
 			// TODO: add generic command handler
-			if ((message.sender === 13391) && ((message.type === bcplus.messageType.NORMAL) || (message.type === bcplus.messageType.WHISPER)) && message.plainText.startsWith('!version')) {
-				var text = 'BCPlus ' + bcplus.getVersion();
+			if ((message.sender === 13391) && ((message.type === bcplus.messageType.NORMAL) || (message.type === bcplus.messageType.WHISPER))) {
 				var awayStatus = $.extend({}, bcplus.getAwayStatus());
+				var handledCommand = false;
 				
-				if (message.type === bcplus.messageType.WHISPER) {
-					// TODO: dynamically get username
-					text = '/whisper ' + message.username + ', ' + text;
+				if (message.plainText.startsWith('!version')) {
+					var text = 'BCPlus ' + bcplus.getVersion();
+					
+					if (message.type === bcplus.messageType.WHISPER) {
+						// TODO: dynamically get username
+						text = '/whisper ' + message.username + ', ' + text;
+					}
+					
+					bcplus.sendMessage(text);
+				}
+				else if (message.plainText.startsWith('!browser')) {
+					var text = ' ' + $.browser.version;
+					
+					if ($.browser.mozilla) {
+						text = 'Mozilla Firefox' + text;
+					}
+					else if ($.browser.chrome) {
+						text = 'Google Chrome' + text;
+					}
+					
+					bcplus.sendMessage(text);
 				}
 				
-				bcplus.sendMessage(text);
-				
-				if (awayStatus.isAway) {
-					text = '/away';
+				if (handledCommand && awayStatus.isAway) {
+					var text = '/away';
 					
 					if (awayStatus.message !== '') {
 						text += ' ' + awayStatus.message;

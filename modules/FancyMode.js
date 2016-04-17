68ae3920a6aee7818245be195b8d9ee2222964fc
Stefan <Stefan_Hahn1@gmx.net>
2016-04-17 19:27:09 +0200

    Added fancy mode
    This alters the username color after every sent message. There's no UI
    to active this feature. This is intendet to keep this as some kind of
    easter egg.

---
+++ modules/FancyMode.js
@@ -0,0 +1,33 @@
+Modules.FancyMode = (function() {
+	var bcplus = null;
+	
+	var initialize = function(_bcplus) {
+		bcplus = _bcplus;
+		
+		addEventListeners();
+	};
+	
+	var addEventListeners = function() {
+		bcplus.addEventListener('messageSubmit', function() {
+			if (bcplus.getStorage().getValue('fancyMode', false)) {
+				bcplus.sendMessage('/color ' + getRandomColor() + ' ' + getRandomColor(), false);
+			}
+		});
+		bcplus.addEventListener('messageAdded', function(messageNodeEvent) {
+			if ((messageNodeEvent.messageType === bcplus.messageType.INFO) && messageNodeEvent.messageText.startsWith('Die Farbe')) {
+				messageNodeEvent.messageNode.addClass('invisible');
+			}
+		});
+	};
+	
+	var getRandomColor = function() {
+		return	'#' + 
+			('00' + Math.floor(Math.random() * 256).toString(16)).slice(-2) + 
+			('00' + Math.floor(Math.random() * 256).toString(16)).slice(-2) + 
+			('00' + Math.floor(Math.random() * 256).toString(16)).slice(-2);
+	};
+	
+	return {
+		initialize:	initialize
+	};
+})();
